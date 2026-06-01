2f1ddc3ad751a0a11defec00a6282d99cdcc6d51
Arun Senapati <arunsenapati407@gmail.com>
2026-06-01 23:11:08 +0530

    add ath service with axios

---
+++ src/services/authService.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API = axios.create({
+    baseURL: "http://localhost:5000/api"
+});
+
+export const registerUser = async (data: {
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+}) => {
+    const response = await API.post("/auth/register", data);
+    return response.data;
+};
+
+export const loginUser = async (data: {
+    email: string;
+    password: string;
+}) => {
+    const response = await API.post("/auth/login", data);
+    return response.data;
+};
